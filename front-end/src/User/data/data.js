e63bf779fdfd344180ad164d1f093946e900aca0
Meera-jose <meerajose2024@gmail.com>
2023-10-09 14:56:28 +0530

    first commit

---
+++ front-end/src/User/data/data.js
@@ -0,0 +1,104 @@
+export const IndoorEvents = [
+  {
+    "name": "Workshops",
+    "value": "Workshops",
+  },
+  
+  {
+    "name": "Museums",
+    "value": "Museums",
+  },
+  {
+    "name": "Movies",
+    "value": "Movies",
+  }
+  
+  
+]
+
+
+
+export const OutdoorEvents = [
+  
+  {
+    "name": "Adventure",
+    "value": "Adventure",
+  },
+  
+  {
+    "name": "Shopping",
+    "value": "Shopping",
+  },
+  {
+    "name": "Nature Park",
+    "value": "Nature Park",
+  }
+  
+]
+
+
+export const IndoorActivity = [
+ 
+  
+  {
+    "name": "Arcades",
+    "value": "Arcades",
+  },
+  {
+    "name": "Board Games",
+    "value": "Board Games",
+  },
+  {
+    "name": "Games",
+    "value": "Games",
+  },
+  {
+    "name": "Fitness",
+    "value": "Fitness",
+  }
+  
+]
+
+export const OutdoorActivity = [
+  
+  {
+    "name": "Hiking",
+    "value": "Hiking",
+  },
+  
+  {
+    "name": "Fishing",
+    "value": "Fishing",
+  },
+  {
+    "name": "Surfing",
+    "value": "Surfing",
+  }
+ 
+]
+
+export const events = [
+  {
+    "activityname": "Margam Park Adventure",
+    "hours": "10:00am - 5:00pm",
+    "days": "Monday - Friday",
+    "price": "6 Pounds",
+    "site": "www.margampark.com",
+    "address": "Margam, Neath Port Talbot",
+    "pindode": "SA13 2TJ",
+    "contact": "(01639) 881 635",
+    "area": "Outdoor",
+    "latitude": 51.51816488036758,
+    "longitude": -0.08583068847656251,
+  },
+  
+ 
+]
+
+export const profileDetails = {
+  name: 'John Doe',
+  email: 'john@example.com',
+  number: '123-456-7890',
+  pincode: '12345',
+  address: '123 Main Street, City, Country',
+};
